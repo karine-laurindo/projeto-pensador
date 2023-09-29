f2c8e8795cc3d1aa8e0c3c50ab591d65dd96b617
karine-laurindo <karine.laurindo3@gmail.com>
2023-09-29 14:41:24 -0300

    Projeto configurado

--- index.js
+++ index.js
@@ -7,3 +7,59 @@ const flash = require('express-flash')
 const app = express()
 
 const conn = require('./db/conn')
+
+//Import models
+
+//Import rotas
+
+//Import Controller
+
+//Configurar a engine
+app.engine('handlebars', exphbs.engine())
+app.set('view wngine', 'handlebars')
+
+//Configurar JSON
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
+
+//Middleware para as sessões
+app.use(session({
+  name: 'session',
+  secret: 'nosso_secret',//Quanto maior a crypto melhor
+  resave: false,
+  saveUninitialized: false,
+  store: new FileStore({
+    logFn: function (){},
+    path: require('path').join(require('os').tmpdir(), 'sessions')
+  }),
+  cookie:{
+    secure: false,
+    maxAge: 360000,
+    expires: new Date(Date.now() +360000),
+    httpOnly: true
+  }
+}))
+
+//Importar as flash
+app.use(flash())
+
+//Importar arquvos estáticos
+app.use(express.static('public'))
+
+//Armazenar as sessões nas rotas
+app.use((req, res, next)=>{
+  if(req.session.userId){
+    res.locals.session = req.session
+  }
+  next()
+})
+
+//Rotas
+
+//Conexão e criação das tabelas do banco
+conn
+.sync()
+.then(()=>{
+  app.listen(3333)
+})
+.catch((err) => console.log(err))
