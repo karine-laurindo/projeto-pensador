2916f3483754352109baeb87f57922a1a95fc0c6
karine-laurindo <karine.laurindo3@gmail.com>
2023-10-06 14:19:41 -0300

    Rota e view home finalizado

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const session = require('express-session')
-const FileStore = require('session-file-store')(session)
+const session = require('express-session')//Criar sessão do usuário
+const Filestore = require('session-file-store')(session)
 const flash = require('express-flash')
 
 const app = express()
 
 const conn = require('./db/conn')
 
-//Import models
+//Models
 const User = require('./models/User')
 const Tought = require('./models/Tought')
 
-//Import rotas
+// importar rotas
+const  toughtRouter = require('./routes/toughtRoutes')
 
-//Import Controller
+// importar controller
+const ToughtController = require('./controllers/ToughtController')
 
-//Configurar a engine
+//Import Engine
 app.engine('handlebars', exphbs.engine())
-app.set('view wngine', 'handlebars')
+app.set('view engine', 'handlebars')
 
-//Configurar JSON
-app.use(express.urlencoded({ extended: true}))
+//Import JSON
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-//Middleware para as sessões
+//Import middleware para controle de sessões
 app.use(session({
-  name: 'session',
-  secret: 'nosso_secret',//Quanto maior a crypto melhor
+  name:"session",
+  secret:"nosso_segredo",
   resave: false,
   saveUninitialized: false,
-  store: new FileStore({
-    logFn: function (){},
+  store: new Filestore({
+    logFn: function() {},
     path: require('path').join(require('os').tmpdir(), 'sessions')
   }),
   cookie:{
     secure: false,
     maxAge: 360000,
-    expires: new Date(Date.now() +360000),
+    expires: new Date(Date.now() + 360000),
     httpOnly: true
   }
 }))
 
-//Importar as flash
+//Import as flash Messages
 app.use(flash())
 
-//Importar arquvos estáticos
+//Import static files
 app.use(express.static('public'))
 
-//Armazenar as sessões nas rotas
-app.use((req, res, next)=>{
-  if(req.session.userId){
-    res.locals.session = req.session
+//Middleware para armazenar sessões na resposta
+app.use((request, response, next)=>{
+  if(request.session.userId){
+    response.locals.session = request.session
   }
   next()
 })
 
-//Rotas
+// rotas
+app.use('/toughts', toughtRouter)
+app.get('/', ToughtController.showToughts)
 
-//Conexão e criação das tabelas do banco
 conn
-.sync()
-.then(()=>{
-  app.listen(3333)
-})
-.catch((err) => console.log(err))
+  .sync()
+  .then(()=>{
+    app.listen(3333)
+  })
+  .catch((err)=>console.log(err))
