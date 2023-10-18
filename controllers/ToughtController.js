7632c476d5b082534f028a77bc37354bec2a4c09
karine-laurindo <karine.laurindo3@gmail.com>
2023-10-18 16:29:28 -0300

    update commit

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -5,12 +5,36 @@ module.exports = class ToughtController{
   static async showToughts(req, res){
   return res.render('toughts/home')
   }
-  static async dashboard(req,res){
-    const UserId = req.session.userId
-  }
   static async dashboard(req, res){
-    return res.render('toughts/dashboard')
+    const userId = req.session.userId
+    //Select join com SEQUELIZE
+    const user = await User.findOne({
+      where:{id:userId},
+      include: Tought,
+      plain:true
+    })
+    if(!user){
+      res.redirect('/login')
+    }
+    // console.log(user.Toughts)
+    const toughts = user.Toughts.map((result)=> result.dataValues)
+    console.log(toughts)
+
+    let emptyTought = false
+
+    if(toughts.length === 0){
+      emptyTought = true
+    }
+
+    return res.render('toughts/dashboard', {toughts})
   }
+  static async deleteToughts(request,response){
+    const id = request.body.id
+
+    await Tought.destroy({where:{id:id}})
+    return response.redirect('/toughts/dashboard')
+  }
+
   static createTought(req, res){
     return res.render('toughts/create')
   }
@@ -30,4 +54,12 @@ module.exports = class ToughtController{
       console.log(`Aconteceu um erro ${error} `)
     }
   }
+  static async editTought(req, res){
+    const id = req.params.id
+
+    const tought = await Tought.findOne({where:{id:id}, raw:true})
+    res.render('toughts/edit', {tought})
+  }
+
+  static async editToughtSave(req,res){}
 }
--- routes/toughtRoutes.js
+++ routes/toughtRoutes.js
@@ -9,7 +9,15 @@ const checkAuth = require('../helpers/auth').checkAuth
 
 router.get('/dashboard', checkAuth, ToughtController.dashboard)
 router.get('/add', checkAuth, ToughtController.createTought)
+
+//Rota para mostrar o formulário
+router.get('/edit/:id', checkAuth, ToughtController.editTought)
+
+//Rota para eidtar o formulário
+router.post('/edit', checkAuth, ToughtController.editToughtSave)
+
 router.post('/add', checkAuth, ToughtController.createToughtSave)
+router.post("/delete", ToughtController.deleteToughts);
 router.get('/', ToughtController.showToughts)
 
 module.exports = router
